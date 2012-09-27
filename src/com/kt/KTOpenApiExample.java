e7a76b475a96701a3a11316cd33ec4bce9f83c52
unikys <sungihk.yang@kt.com>
2012-09-27 16:42:07 +0900

    0927 : added ids

--- src/com/kt/KTOpenApiExample.java
+++ src/com/kt/KTOpenApiExample.java
@@ -8,7 +8,7 @@ public class KTOpenApiExample {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		KTOpenApiManager apiManager = new KTOpenApiManager("AuthKey", "SecretKey"); 
+		KTOpenApiManager apiManager = new KTOpenApiManager("8dfad1945247e11e7f7d813647a3516b", "c65453cf50d82f0865ed747b2f3b76fe"); 
 		new KTOpenApiGUI(apiManager);
 
 	}
